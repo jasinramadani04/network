78fc8af6a3186933dea913f5f78d20b6e32bb827
jasinramadani04 <76982137+jasinramadani04@users.noreply.github.com>
2023-06-23 10:55:20 +0200

    Update network.py

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import socket
 import requests
 import json
 from termcolor import colored
@@ -20,12 +21,39 @@ def get_internet_service_provider():
 def print_stylish(text):
     print(colored(text, 'cyan', attrs=['bold', 'underline']))
 
+def scan_ports(ip, ports):
+    open_ports = []
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            open_ports.append(port)
+        sock.close()
+    return open_ports
+
+def get_connected_users(ip, ports):
+    users = []
+    for port in ports:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            sock.connect((ip, port))
+            sock.sendall(b'WHO')
+            response = sock.recv(1024)
+            users.extend(response.decode().split(','))
+            sock.close()
+        except:
+            pass
+    return list(set(users))
+
 def main():
     isp = get_internet_service_provider()
     ip = get_public_ip()
     location_data = get_location_info()
     latitude = location_data['lat']
     longitude = location_data['lon']
+    open_ports = scan_ports(ip, [80, 443, 8080])  # Modify the list of ports to scan as per your requirements
 
     print_stylish('Internet Service Provider:')
     print(isp)
@@ -33,6 +61,17 @@ def main():
     print(ip)
     print_stylish('Geographic Location:')
     print(f'Latitude: {latitude}, Longitude: {longitude}')
+
+    if len(open_ports) > 0:
+        print_stylish('Connected Users:')
+        users = get_connected_users(ip, open_ports)
+        if len(users) > 0:
+            for user in users:
+                print(user)
+        else:
+            print('No users connected to the server.')
+    else:
+        print('No open ports found.')
 
 if __name__ == '__main__':
     main()
